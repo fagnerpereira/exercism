8bee4623b1e5ecd72964c2c6733fc40c146d8637
Fagner <fagnerfpr@gmail.com>
2024-07-14 00:51:07 -0300

    refactoring word count

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -6,20 +6,6 @@ class Phrase < WordCount
   end
 
   def word_count
-    delimiters = [' ', "\n", "\t", ',', ';', '.', '!', '?', '"', '(', ')']
-    regex = Regexp.union(delimiters)
-    counts = Hash.new(0)
-
-    @phrase.downcase.split(regex).each do |word|
-      word.gsub!(/[^a-zA-Z0-9']/, '')
-      word.sub!(/^'/, '')
-      word.sub!(/'$/, '')
-
-      next if word.empty?
-
-      counts[word] += 1
-    end
-
-    counts
+    @phrase.downcase.scan(/\b[\w']+\b/).tally
   end
 end
