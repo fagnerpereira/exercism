aabed3b9a6ef46d0322cfc002c82ce9679a266fa
Fagner <fagnerfpr@gmail.com>
2024-07-13 22:49:58 -0300

    refactoring two buckets in ruby

--- ruby/two-buckets/two_bucket.rb
+++ ruby/two-buckets/two_bucket.rb
@@ -1,7 +1,8 @@
 class Bucket
-  attr_accessor :capacity, :usage
+  attr_accessor :capacity, :usage, :name
 
-  def initialize(capacity)
+  def initialize(capacity, name)
+    @name = name
     @capacity = capacity
     @usage = 0
   end
@@ -32,59 +33,64 @@ class Bucket
   def full?
     usage == capacity
   end
+
+  def reached?(goal)
+    usage == goal
+  end
 end
 
 class TwoBucket
   attr_reader :moves
 
   def initialize(bucket_one_size, bucket_two_size, goal, start_bucket)
-    @bucket_one = Bucket.new(bucket_one_size)
-    @bucket_two = Bucket.new(bucket_two_size)
+    @bucket_one, @bucket_two = initialize_buckets(bucket_one_size, bucket_two_size, start_bucket)
     @goal = goal
-    @start_bucket = start_bucket
     @moves = 0
 
-    run
+    solve!
   end
 
-  def run
-    return if @bucket_one.usage == @goal || @bucket_two.usage == @goal
+  def initialize_buckets(bucket_one_size, bucket_two_size, start_bucket)
+    if start_bucket == 'one'
+      [Bucket.new(bucket_one_size, 'one'), Bucket.new(bucket_two_size, 'two')]
+    else
+      [Bucket.new(bucket_two_size, 'two'), Bucket.new(bucket_one_size, 'one')]
+    end
+  end
 
-    @moves += 1
-    if @start_bucket == 'one'
-      if @bucket_one.empty?
-        @bucket_one.fillup!
-      elsif @bucket_two.free.positive?
-        if @goal == @bucket_two.capacity
-          @bucket_two.fillup!
-        else
-          @bucket_one.pour!(@bucket_two)
-        end
+  def solve!
+    loop do
+      break if goal_bucket
+
+      perform_move
+    end
+  end
+
+  def perform_move
+    if @bucket_one.empty?
+      @bucket_one.fillup!
+    elsif @bucket_two.free.positive?
+      if @goal == @bucket_two.capacity
+        @bucket_two.fillup!
       else
-        @bucket_two.clear!
+        @bucket_one.pour!(@bucket_two)
       end
     else
-      if @bucket_two.empty?
-        @bucket_two.fillup!
-      elsif @bucket_one.free.positive?
-        if @goal == @bucket_one.capacity
-          @bucket_one.fillup!
-        else
-          @bucket_two.pour!(@bucket_one)
-        end
-      else
-        @bucket_one.clear!
-      end
+      @bucket_two.clear!
     end
 
-    run
+    @moves += 1
   end
 
   def goal_bucket
-    @goal == @bucket_one.usage ? 'one' : 'two'
+    return @bucket_one.name if @bucket_one.reached?(@goal)
+
+    @bucket_two.name if @bucket_two.reached?(@goal)
   end
 
   def other_bucket
-    @goal == @bucket_one.usage ? @bucket_two.usage : @bucket_one.usage
+    return @bucket_one.usage if @bucket_two.reached?(@goal)
+
+    @bucket_two.usage if @bucket_one.reached?(@goal)
   end
 end
