f381a6e0ec520e1d81181e572294345162641e9b
Fagner Pereira <fagnerfpr@gmail.com>
2024-11-22 18:54:45 -0300

    refactoring

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,20 +1,13 @@
 class Raindrops
   def self.convert(n)
-    result = ""
-
     {3 => "Pling", 5 => "Plang", 7 => "Plong"}.select do |d, s|
-      if n % d == 0
-        result += s
+      n % d == 0
+    end.values.then do |result|
+      if result.empty?
+        n.to_s
+      else
+        result.join
       end
-    end
-
-    if result.empty?
-      n.to_s
-    else
-      result
     end
   end
 end
-
-puts Raindrops.convert 30
-puts Raindrops.convert 4
